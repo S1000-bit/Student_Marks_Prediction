1dfb405cb318b5ada8c1e2a496c1f8a2bb9ef533
Poojary - <poojary.-@stud.th-deg.de>
2025-05-22 14:31:34 +0200

    completed hyperparameter tuning (moodel_trainer.py)

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import pandas as pd
 from src.exception import CustomException
 import dill
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path,obj):
     try:
@@ -18,7 +19,7 @@ def save_object(file_path,obj):
     except Exception as e:
         raise CustomException(e,sys)
     
-def evaluate_models(X_train, y_train,X_test,y_test,models,param):
+def evaluate_model(X_train, y_train,X_test,y_test,models,param):
     try:
         report = {}
 
