21517d515623d01727f40b619a35f03a3c0467e1
Poojary - <poojary.-@stud.th-deg.de>
2025-05-22 15:26:37 +0200

    updated utils and requirements.txt

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 from src.exception import CustomException
 import dill
+import pickle
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
 
